cad85c26d30fb2439a2147bb4788a41e645816ad
Joe Johnson <joe.wendell.johnson@gmail.com>
2018-02-09 12:12:31 -0600

    Added In Memory DB, removed mock.  Started services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,13 @@ export class HeroesComponent implements OnInit {
         this.getHeroes();
     }
     heroes: Hero[];
+    
     getHeroes(): void {
         this.heroService.getHeroes().subscribe(results => {this.heroes = results});
+    }
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.addHero({ name } as Hero).subscribe(hero => {this.heroes.push(hero);});
     }
 }
